b49cc459a4c2c2bb631ad5428419fd79327ea47b
Matheus Oliveira <matheus.oliveira6842@gmail.com>
2023-12-11 19:36:34 -0300

    .

--- aula04exercicios/script.js
+++ aula04exercicios/script.js
@@ -2,19 +2,23 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora}horas`
-    if(hora >= 0 && hora <12){
+    //var hora = data.getHours()
+    var hora = 0
+    if(hora >= 6 && hora <12){
         img.src ='amanhecereditado.png'
-        
+        document.body.style.background = '#F3D667'
+        msg.innerHTML = `Agora são ${ hora } horas tenha um otimo dia`
 
     } 
     else if(hora >= 12 && hora < 18)
     {
         img.src='entardecereditado1.png'
+        document.body.style.background = '#E5470F'
+        msg.innerHTML = `Agora são ${ hora } horas tenha uma otima tarde`
     }else {
         img.src='noiteeditado.png'
         document.body.style.background = '#4C5250'
+        msg.innerHTML = `Agora são ${ hora } horas tenha uma otima noite`
 
     }
 }
